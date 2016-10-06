2f9f208318d03d0df4f453a68c145eb0c5903318
clara <clarajames@minneapolis.edu>
2016-10-06 11:10:37 -0500

    Search the RhymeBrain api to find rhyming words

--- RhymesWithOrange/Form1.Designer.cs
+++ RhymesWithOrange/Form1.Designer.cs
@@ -28,12 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.getRhymesButton = new System.Windows.Forms.Button();
+            this.wordTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.resultsLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // getRhymesButton
+            // 
+            this.getRhymesButton.Location = new System.Drawing.Point(26, 40);
+            this.getRhymesButton.Name = "getRhymesButton";
+            this.getRhymesButton.Size = new System.Drawing.Size(214, 23);
+            this.getRhymesButton.TabIndex = 0;
+            this.getRhymesButton.Text = "What rhymes with Orange?";
+            this.getRhymesButton.UseVisualStyleBackColor = true;
+            this.getRhymesButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // wordTextBox
+            // 
+            this.wordTextBox.Location = new System.Drawing.Point(108, 14);
+            this.wordTextBox.Name = "wordTextBox";
+            this.wordTextBox.Size = new System.Drawing.Size(132, 20);
+            this.wordTextBox.TabIndex = 1;
+            this.wordTextBox.Text = "Orange";
+            this.wordTextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter a word ";
+            // 
+            // resultsLabel
+            // 
+            this.resultsLabel.AutoSize = true;
+            this.resultsLabel.Location = new System.Drawing.Point(20, 80);
+            this.resultsLabel.Name = "resultsLabel";
+            this.resultsLabel.Size = new System.Drawing.Size(0, 15);
+            this.resultsLabel.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.resultsLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.wordTextBox);
+            this.Controls.Add(this.getRhymesButton);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button getRhymesButton;
+        private System.Windows.Forms.TextBox wordTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label resultsLabel;
     }
 }
 
--- RhymesWithOrange/Form1.cs
+++ RhymesWithOrange/Form1.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +18,82 @@ namespace RhymesWithOrange
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string buttonTextTemplate = "What rhymes with '{0}'?";
+            string word = wordTextBox.Text;
+            getRhymesButton.Text = String.Format(buttonTextTemplate, word);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            resultsLabel.Text = "Searching....";
+            string word = wordTextBox.Text;
+            List<String> rhymes = findRhymes(word);
+            if (rhymes != null)
+            {
+                resultsLabel.Text = createDisplayString(rhymes);
+            }
+            else
+            {
+                resultsLabel.Text = "Oh no! An error fetching rhymes";
+            } 
+        }
+
+        private string createDisplayString(List<string> rhymes)
+        {
+
+            string results = "Here are some words that rhyme:\n";
+            foreach (String word in rhymes)
+            {
+                results += word;
+                results += "\n";
+            }
+
+            return results;
+        }
+
+        private List<String> findRhymes(string word)
+        {
+            try
+            {
+                String urlBase = "http://rhymebrain.com/talk?function=getRhymes&word={0}&maxResults=10";
+                String url = String.Format(urlBase, word);
+
+                WebRequest request = WebRequest.Create(url);
+                WebResponse resp = request.GetResponse();
+
+                Stream data = resp.GetResponseStream();
+                StreamReader reader = new StreamReader(data);
+
+                string json = reader.ReadToEnd();
+
+                resp.Close();
+
+                List<Rhyme> rhymeObjectsList = JsonConvert.DeserializeObject<List<Rhyme>>(json);
+
+                List<string> rhymingWordsList = new List<string>();
+
+                foreach (Rhyme rhyme in rhymeObjectsList)
+                {
+                    rhymingWordsList.Add(rhyme.word);
+                }
+
+                return rhymingWordsList;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error fetching rhymes: " + e);
+                return null;
+            }
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+        
         }
     }
 }
